38d3affbc1807c93ad0f55b765b393cf8e15ec7b
Ali Shah <ali1195shah@gmail.com>
2019-08-25 03:48:16 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -11,8 +11,12 @@ end
 
 def find_max_value(array)
   # Add your solution here
+  array.sort!
+  return array[-1]
 end
 
 def find_min_value(array)
   # Add your solution here
+  array.sort!
+  return array[0]
 end
